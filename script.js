1d62c39898b4fb2fb572e776d6ba971010a6d54e
Dipak 2056 <paudelsantosh508@gmail.com>
2022-02-21 10:05:09 +1100

    now we can find the user searched and the number

--- script.js
+++ script.js
@@ -1,77 +1,76 @@
 const   apiurl= 'https://randomuser.me/api/?';
 const listElm = document.querySelector('#list');
+let userArgs = [];
+const displayUSers = (args = userArgs) =>{
+let str = '';
+    args.map((usr)=>{
+        str += ` <div class="col-md-6 col-lg-3 py-3">
+        <div class="card " style="min-width: 18rem;">
+            <img src="${usr.picture.large}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">
+              ${usr.name.title}
+              ${usr.name.first}
+              ${usr.name.last}
+              </h5>
+              <p class="card-text">
+              <ul class="list-unstyled">
 
+  
+    <li>
+    <span class="deco">
+    <i class="fa-solid fa-mobile"></i></span> ${usr.phone}
+    </li>
+    <li>
+    <span class="deco">
+    <i class="fa-solid fa-calendar"></i></span> ${usr.email}
+    </li>
+    <span class="deco">
+    <i class="fa-solid fa-map"></i></span> ${usr.dob.date}
+    </li>
+   
+</ul>
+              
+              </p>
+            </div>
+          </div>
+    </div>
+    `
+    })
+  
+listElm.innerHTML = str;
+
+}
 const fetchUsers = (params = 'results=20') => {
-
-    fetch(apiurl+params).then(respose =>respose.json())
+fetch(apiurl+params).then(respose =>respose.json())
     .then(data => {
-
-        const user = data.results;
-
-        let names = [];
-        
-        for(i=0;i<user.length;i++){
-             names.push(user[i].name.first+user[i].name.last);
-        }
-        console.log(names);
-        
-        let str = '';
-        user.map((usr)=>{
-            str += ` <div class="col-md-6 col-lg-3 py-3">
-            <div class="card " style="min-width: 18rem;">
-                <img src="${usr.picture.large}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">
-                  ${usr.name.title}
-                  ${usr.name.first}
-                  ${usr.name.last}
-                  </h5>
-                  <p class="card-text">
-                  <ul class="list-unstyled">
-
-      
-        <li>
-        <span class="deco">
-        <i class="fa-solid fa-mobile"></i></span> ${usr.phone}
-        </li>
-        <li>
-        <span class="deco">
-        <i class="fa-solid fa-calendar"></i></span> ${usr.email}
-        </li>
-        <span class="deco">
-        <i class="fa-solid fa-map"></i></span> ${usr.dob.date}
-        </li>
-       
-    </ul>
-                  
-                  </p>
-                </div>
-              </div>
-        </div>
-        `
-        })
-      
-    listElm.innerHTML = str;
+            userArgs = data.results;
+        displayUSers()
 
     });
 }
 
 fetchUsers();
+//on changing the value on the select box
 const handleonchange = (e) => {
     const params = "results=20&gender="+e.value;
    fetchUsers(params);
 
 }
-let searchingName = [];
-const  handleonType = (e) => {
-searchingName.push(e.value);
-if (searchingName.length > 1){
-    searchingName.shift();
-    
-}
 
-}
+const handleOnSearch = () => {
+    const spanshow = document.getElementById('result');
+    spanshow.className = 'd-block toggle'
+ const searchStr = document.getElementById('search').value;
+    const filteredUser = userArgs.filter(item => {
+        const userName = `${item?.name.first} ${item?.name.last}`
+        if(userName.toLocaleLowerCase().includes(searchStr.toLocaleLowerCase())){
+            return item;
+        }
 
-const findingName =() => {
-
+    });
+    displayUSers(filteredUser);
+     total = filteredUser.length;
+     console.log(total);
+     document.getElementById('foundnumber').innerText = total;
 }
